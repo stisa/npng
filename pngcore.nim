59aa5bcda93af074cea873f052fbc06a05e98986
Silvio T <stisa4@gmail.com>
2016-09-21 22:27:53 +0200

    only idat missing

--- pngcore.nim
+++ pngcore.nim
@@ -20,13 +20,17 @@ type IHDR = object of Chunk
   filter_type:uint8 # only 0 is defined
   interlace_type:uint8 # 0 and 1 are defined
 
-proc crc32(sta: uint32, data:openarray[uint8]):uint32 =
+type IDAT = object of Chunk
+
+type IEND = object of Chunk
+
+proc crc32(sta: uint32, data:openarray[char]):uint32 =
   var state = sta
   let len = data.len
   state = not state;
   for i in 0..<len:
     for j in 0..<8: # TODO: table-based
-      let bit : uint32 = (state xor (data[i] shr j.uint8)) and 1;
+      let bit : uint32 = (state xor (data[i].uint8 shr j.uint8)) and 1;
       state = (state shr 1) xor ((high(uint32)+1-bit).int64 and 0xEDB88320).uint32
   return not state;
 
@@ -53,21 +57,30 @@ proc bytesOf(b4:uint32):array[4,uint8]=
   result[2] = (b4 shr 16).uint8
   result[3] = (b4 shr 24).uint8
 
-proc charOf(b4:uint32):array[4,char]=
-  result[0] = (b4 shr 0).char
-  result[1] = (b4 shr 8).char
-  result[2] = (b4 shr 16).char
-  result[3] = (b4 shr 24).char
+proc charsOf(b4:uint32):array[4,char]=
+  result[0] = (b4 shr 24).uint8.char
+  result[1] = (b4 shr 16).uint8.char
+  result[2] = (b4 shr 8).uint8.char
+  result[3] = (b4 shr 0).uint8.char
+  #echo repr result
 
-method serialize(chunk: Chunk):seq[uint8] {.base.} =
-  result = newSeq[uint8]()
-  result &= bytesOf(chunk.bytelen)
-  let chartoint8 = [chunk.typ[0].uint8,chunk.typ[1].uint8,chunk.typ[2].uint8,chunk.typ[3].uint8]
-  echo repr chartoint8
-  echo repr (['I'.uint8,'H'.uint8,'D'.uint8,'R'.uint8])
-  result &= chartoint8
-  result&=chunk.data
-  result&= bytesOf(crc32(0, chartoint8&chunk.data))
+proc insertChars(s:var string, chrs:openarray[char])=
+  if s==nil: s=""
+  for c in chrs: s&=c
+
+method serialize(chunk: Chunk):string {.base.}=#seq[char] {.base.} =
+  #result = newSeq[char]()
+  result=""
+  result.insertChars(charsOf(chunk.bytelen))
+  #echo repr result
+  #let chartoint8 = [chunk.typ[0],chunk.typ[1],chunk.typ[2],chunk.typ[3]]
+  result &= chunk.typ # chartoint8
+  if chunk.data.len>0:
+    result.insertChars(chunk.data)
+    #for cd in chunk.data: result&=cd
+    result.insertChars( charsOf(crc32(0, chunk.typ&chunk.data)))
+  else: result&= charsOf(crc32(0, chunk.typ))
+  #echo repr result
 
 proc initIHDR(w,h:int,bit_depth=8,ctype=6,comptype=0,ftype=0,itype=0):IHDR =
   # IHDR specific data
@@ -81,20 +94,28 @@ proc initIHDR(w,h:int,bit_depth=8,ctype=6,comptype=0,ftype=0,itype=0):IHDR =
   # General chunk
   result.bytelen = 3*4 + 2*4 + 5
   result.typ = "IHDR"
-  result.data = bytesOf(result.width)&bytesOf(result.height)
-  result.data &= result.bit_depth
-  result.data &= result.color_type
-  result.data &= result.compression_type
-  result.data &= result.filter_type
-  result.data &= result.interlace_type
+  result.data = charsOf(result.width)&charsOf(result.height)
+  result.data &= result.bit_depth.char
+  result.data &= result.color_type.char
+  result.data &= result.compression_type.char
+  result.data &= result.filter_type.char
+  result.data &= result.interlace_type.char
+
+proc initIDAT() = discard
+
+proc initIEND():IEND=
+  result.bytelen = 4+4
+  result.typ = "IEND"
 
 when isMainModule:
   var fs = newFileStream("out.txt",fmWrite)
   fs.write(Header)
   let ihdr = initIHDR(3,2)
-  let sihdr = serialize(ihdr)
-  echo sihdr
-  fs.write(sihdr[4..7])
+  let iend = initIEND()
+  fs.write(serialize(ihdr))
+  echo repr serialize(ihdr), "<!IHDR!>"
+  fs.write(serialize(iend))
+  echo repr serialize(iend), "<!IEND!>"
   fs.close()
 
   echo repr readFile("out.txt")
