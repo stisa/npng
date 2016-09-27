1094e358482553ad7833b5f7663a71e4764185a3
Silvio T <stisa4@gmail.com>
2016-09-27 17:26:48 +0200

    got it working

--- pngcore.nim
+++ pngcore.nim
@@ -47,7 +47,17 @@ proc crc32(sta: uint32, data:openarray[char]):uint32 =
       state = (state shr 1) xor ((high(uint32)+1-bit).int64 and 0xEDB88320).uint32
   return not state;
 
-proc adler32(state: var uint32, data:openarray[uint8]):uint32 =
+proc adler32(data:openarray[char]):uint32 = # output is correct
+  let len = data.len
+  var s1 : uint32 = 1
+  var s2 : uint32 = 0
+  for i in 0..<len:
+    s1 = (s1 + data[i].uint32) mod 65521;
+    s2 = (s2 + s1) mod 65521;
+
+  return s2.uint32 shl 16 or s1;
+
+proc oldadler32(state: uint32, data:openarray[uint8]):uint32 =
   let len = data.len
   var s1 : uint16 = (state shr  0).uint16
   var s2 : uint16 = (state shr 16).uint16
@@ -65,10 +75,10 @@ proc `&`[T](ar,ar2:openarray[T]):seq[T]=
 
 
 proc bytesOf(b4:uint32):array[4,uint8]=
-  result[0] = (b4 shr 0).uint8
-  result[1] = (b4 shr 8).uint8
-  result[2] = (b4 shr 16).uint8
-  result[3] = (b4 shr 24).uint8
+  result[0] = (b4 shr 24).uint8
+  result[1] = (b4 shr 16).uint8
+  result[2] = (b4 shr 8).uint8
+  result[3] = (b4 shr 0).uint8
 
 proc charsOf(b4:uint32):array[4,char]=
   result[0] = (b4 shr 24).uint8.char
@@ -92,8 +102,9 @@ method serialize(chunk: Chunk):string {.base.}=#seq[char] {.base.} =
     result.insertChars(chunk.data)
     #for cd in chunk.data: result&=cd
     result.insertChars( charsOf(crc32(0, chunk.typ&chunk.data)))
-  else: result&= charsOf(crc32(0, chunk.typ))
-  #echo repr result
+  else: 
+    result&= charsOf(crc32(0, chunk.typ))
+ #echo repr result
 
 proc initIHDR(w,h:int,bit_depth=8,ctype=6,comptype=0,ftype=0,itype=0):IHDR =
   # IHDR specific data
@@ -105,7 +116,7 @@ proc initIHDR(w,h:int,bit_depth=8,ctype=6,comptype=0,ftype=0,itype=0):IHDR =
   result.filter_type = ftype.uint8
   result.interlace_type = itype.uint8
   # General chunk
-  result.bytelen = 3*4 + 2*4 + 5
+  result.bytelen = 2*4 + 5
   result.typ = "IHDR"
   result.data = charsOf(result.width)&charsOf(result.height)
   result.data &= result.bit_depth.char
@@ -114,10 +125,12 @@ proc initIHDR(w,h:int,bit_depth=8,ctype=6,comptype=0,ftype=0,itype=0):IHDR =
   result.data &= result.filter_type.char
   result.data &= result.interlace_type.char
 
+
 proc expand(pixels:openarray[Color],w,h:int):seq[seq[char]] =
   ## Expand pixels int scanlines of bytes
   ## Assumes that pixels are 4-bytes ( so r,g,b,a )
   result = newSeq[seq[char]](h)
+  
   var offset = 0
   for yline in result.mitems:
     yline = newSeq[char]()
@@ -134,13 +147,46 @@ proc strToSeqChar(sc:string):seq[char] =
   result = newSeq[char]()
   for c in sc: result&=c
 
+proc strToSeqByte(sc:string):seq[uint8] =
+  result = newSeq[uint8]()
+  for c in sc: result&=c.uint8
+
 proc applyFilter(pixbytes: var seq[seq[char]],filter:Filters=None)=
   if filter == None:
     for ln in pixbytes.mitems:
       ln = 0.char & ln
 
-proc compress(pixbytes: var seq[seq[char]]) =
-  discard
+proc charsOfAndCompl(b2:uint16):array[4,char]=
+  result[0] = (b2 shr 0).uint8.char
+  result[1] = (b2 shr 8).uint8.char
+  echo repr result[0]
+  echo repr result[1]
+  result[2] = ((b2 xor 0xFFFF) shr 0).uint8.char
+  result[3] = ((b2 xor 0xFFFF) shr 8).uint8.char
+
+proc charsOf(b2:uint16):array[2,char]=
+  result[0] = (b2 shr 8).uint8.char
+  result[1] = (b2 shr 0).uint8.char
+
+
+#zlib magic headers
+#78 01 - No Compression/low
+#78 9C - Default Compression
+#78 DA - Best Compression 
+proc compress(pixbytes: var seq[seq[char]]) :seq[char] =
+  var ss = newStringStream()
+  let header = [120.char, 1.char, 1.char]  # magic header + a single block (1) raw (00)
+  let pbytes = deexpand(pixbytes)
+  let adl = charsOf(adler32(pbytes))
+  ss.write(header)
+  echo pbytes.len.uint16
+  ss.write(charsOfAndCompl(pbytes.len.uint16)) # length of compressed block
+  ss.write(pbytes) # compressed data
+  ss.write(adl) # adler32
+  result = ss.data.strToSeqChar
+  ss.close()
+
+  echo repr result
  
 proc zcompress*(data: string): string = # TODO: drop zlib
   let size = data.len
@@ -158,7 +204,8 @@ proc initIDAT(ihdr:IHDR, pixels:openarray[Color]):IDAT =
   let w = ihdr.width*4 # Convert the width from pixels to bytes ( each pixel is 3 bytes of rbg and 1 byte for alpha)
 
   # Begin with image scanlines represented as described in Image layout;
-  # the layout and total size of this raw data are determined by the fields of IHDR. 
+  # the layout and total size of this raw data are determined by the fields of IHDR.
+
   var exppixels = expand(pixels,ihdr.width.int,ihdr.height.int)
   
   # Filter the image data according to the filtering method specified by the IHDR chunk. 
@@ -166,31 +213,30 @@ proc initIDAT(ihdr:IHDR, pixels:openarray[Color]):IDAT =
   # prepending a filter type byte to each scanline.) 
   # http://www.libpng.org/pub/png/spec/1.2/PNG-Filters.html <- Filters
   exppixels.applyFilter(None) # Currently default to none, will need to support others
-
   # Compress the filtered data using the compression method specified by the IHDR chunk.
-  result.data = zcompress(deexpand(exppixels)).strToSeqChar
+  result.data = compress(exppixels) #& adler32(0,deexpand(exppixels).strToSeqByte).charsOf() # maybe cast?
   result.typ = "IDAT"
-  result.bytelen = (4+4+result.data.len).uint32
+  result.bytelen = (result.data.len).uint32
 
 proc initIEND():IEND=
-  result.bytelen = 4+4
+  result.bytelen = 0
   result.typ = "IEND"
 
 when isMainModule:
-  var fs = newFileStream("out.txt",fmWrite)
+  var fs = newFileStream("rout.png",fmWrite)
 
-  let pix = [White, White,White, White,White, White]
-  
+  let pix = [ Color(0xFF0000FF),White,White,White,White,White]
   fs.write(Header)
+
   let ihdr = initIHDR(3,2)
   let iend = initIEND()
   let idat = initIDAT(ihdr,pix)
   fs.write(serialize(ihdr))
-  echo repr serialize(ihdr), "<!IHDR!>"
+  
   fs.write(serialize(idat))
-  echo repr serialize(idat), "<!IDAT!>"  
+  
   fs.write(serialize(iend))
-  echo repr serialize(iend), "<!IEND!>"
+  
   fs.close()
 
-  echo repr readFile("out.txt")
+
--- spec.md
+++ spec.md
@@ -1,3 +1,5 @@
+https://github.com/PistonDevelopers/image-png/blob/master/src/encoder.rs
+
 Critical chunks
 ---------------
 (must appear in this order)
@@ -73,3 +75,145 @@ Color type:       1 byte
 Compression type: 1 byte  
 Filter type:      1 byte  
 Interlace type:   1 byte  
+
+
+# Deflate
+Stream format[edit]
+A Deflate stream consists of a series of blocks. Each block is preceded by a 3-bit header:
+First bit: Last-block-in-stream marker: 
+  1: this is the last block in the stream.
+  0: there are more blocks to process after this one.
+Second and third bits: Encoding method used for this block type: 
+  00: a stored/raw/literal section, between 0 and 65,535 bytes in length.
+  01: a static Huffman compressed block, using a pre-agreed Huffman tree.
+  10: a compressed block complete with the Huffman table supplied.
+  11: reserved, don't use.
+
+# zlib
+Deflate-compressed datastreams within PNG are stored in the "zlib" format,
+which has the structure: 
+   Compression method/flags code: 1 byte
+   Additional flags/check bits:   1 byte
+   Compressed data blocks:        n bytes
+   Check value:                   4 bytes
+
+
+# 3.2.4. Non-compressed blocks (BTYPE=00)
+Any bits of input up to the next byte boundary are ignored. The rest of the block consists of the following information: 
+
+  0   1   2   3   4...
+
++---+---+---+---+================================+
+
+|  LEN  | NLEN  |... LEN bytes of literal data...|
+
++---+---+---+---+================================+
+
+LEN is the number of data bytes in the block. NLEN is the one's complement of LEN. 
+
+# Zlib data format
+2.2. Data format
+
+  A zlib stream has the following structure:
+
+        0   1
+      +---+---+
+      |CMF|FLG|   (more-->)
+      +---+---+
+
+  (if FLG.FDICT set)
+
+        0   1   2   3
+      +---+---+---+---+
+      |     DICTID    |   (more-->)
+      +---+---+---+---+
+
+  (end if)
+
+      +=====================+---+---+---+---+
+      |...compressed data...|    ADLER32    |
+      +=====================+---+---+---+---+
+
+  Any data which may appear after ADLER32 are not part of the zlib
+  stream.
+
+  CMF (Compression Method and flags)
+      This byte is divided into a 4-bit compression method and a 4-
+      bit information field depending on the compression method.
+
+        bits 0 to 3  CM     Compression method
+        bits 4 to 7  CINFO  Compression info
+
+  CM (Compression method)
+      This identifies the compression method used in the file. CM = 8
+      denotes the "deflate" compression method with a window size up
+      to 32K.  This is the method used by gzip and PNG (see
+      references [1] and [2] in Chapter 3, below, for the reference
+      documents).  CM = 15 is reserved.  It might be used in a future
+      version of this specification to indicate the presence of an
+      extra field before the compressed data.
+
+  CINFO (Compression info)
+      For CM = 8, CINFO is the base-2 logarithm of the LZ77 window
+      size, minus eight (CINFO=7 indicates a 32K window size). Values
+      of CINFO above 7 are not allowed in this version of the
+      specification.  CINFO is not defined in this specification for
+      CM not equal to 8.
+
+  FLG (FLaGs)
+      This flag byte is divided as follows:
+
+        bits 0 to 4  FCHECK  (check bits for CMF and FLG)
+        bit  5       FDICT   (preset dictionary)
+        bits 6 to 7  FLEVEL  (compression level)
+
+      The FCHECK value must be such that CMF and FLG, when viewed as
+      a 16-bit unsigned integer stored in MSB order (CMF*256 + FLG),
+      is a multiple of 31.
+
+  FDICT (Preset dictionary)
+      If FDICT is set, a DICT dictionary identifier is present
+      immediately after the FLG byte. The dictionary is a sequence of
+      bytes which are initially fed to the compressor without
+      producing any compressed output. DICT is the Adler-32 checksum
+      of this sequence of bytes (see the definition of ADLER32
+      below).  The decompressor can use this identifier to determine
+      which dictionary has been used by the compressor.
+
+  FLEVEL (Compression level)
+      These flags are available for use by specific compression
+      methods.  The "deflate" method (CM = 8) sets these flags as
+      follows:
+
+        0 - compressor used fastest algorithm
+        1 - compressor used fast algorithm
+        2 - compressor used default algorithm
+        3 - compressor used maximum compression, slowest algorithm
+
+      The information in FLEVEL is not needed for decompression; it
+      is there to indicate if recompression might be worthwhile.
+
+  compressed data
+      For compression method 8, the compressed data is stored in the
+      deflate compressed data format as described in the document
+      "DEFLATE Compressed Data Format Specification" by L. Peter
+      Deutsch. (See reference [3] in Chapter 3, below)
+
+      Other compressed data formats are not specified in this version
+      of the zlib specification.
+
+  ADLER32 (Adler-32 checksum)
+      This contains a checksum value of the uncompressed data
+      (excluding any dictionary data) computed according to Adler-32
+      algorithm. This algorithm is a 32-bit extension and improvement
+      of the Fletcher algorithm, used in the ITU-T X.224 / ISO 8073
+      standard. See references [4] and [5] in Chapter 3, below)
+
+      Adler-32 is composed of two sums accumulated per byte: s1 is
+      the sum of all bytes, s2 is the sum of all s1 values. Both sums
+      are done modulo 65521. s1 is initialized to 1, s2 to zero.  The
+      Adler-32 checksum is stored as s2*65536 + s1 in most-
+      significant-byte first (network) order.
+
+
+
