7d159b160dc91d07363f5a230f821909023072c6
Silvio T <stisa4@gmail.com>
2016-10-01 09:11:33 +0200

    trying to fix js

--- npng.nim
+++ npng.nim
@@ -1,5 +1,5 @@
 import private/impl
-import streams
+import streams,base64
 
 
 type Color* = uint32
@@ -43,22 +43,50 @@ proc initPNG*(w,h:int,pix:seq[Color]):PNG=
   elif pix.len<(w*h):
     result.pixels = pix & newSeq[Color](w*h-pix.len)
 
-proc writeToFile*(png:PNG,file:string)=
-  ## Write a png to a file.
-  ## The file is created if not present and overwritten otherwise.
-  var fs = newFileStream(file,fmWrite)
-  fs.write(encodePng(png.w,png.h,png.pixels))
-  fs.close()
-
 proc fillWith*(png:var PNG,color:Color=White)=
   ## Fill a png with ``color``
   for p in png.pixels.mitems: p=color 
 
-when isMainModule:
-  var png = initPNG(300,300)
-  png.fillWith(BLue)
-  png.pixels[2]=Red
+proc stringify(png:PNG):string {.inline.}= encodePng(png.w,png.h,png.pixels)
+
+proc writeBase64(png:PNG):string=
+  ## Write a png file as base64, useful for inlining
+  ## images in js.
+  result = "data:image/png;base64,"&base64.encode(stringify(png))
+
+when not defined(js):
+  proc writeToFile*(png:PNG,file:string)=
+    ## Write a png to a file.
+    ## The file is created if not present and overwritten otherwise.
+    var fs = newFileStream(file,fmWrite)
+    fs.write(stringify(png))
+    fs.close()
+    #var fs2 = newFileStream("t.txt",fmWrite)
+    #fs2.write("data:image/png;base64,"&base64.encode(stringify(png)))
+    #fs2.close()
+
+const rs = slurp("test2.png")
+
+
+when isMainModule and not defined js:
+  var png = initPNG(3,3)
+  png.fillWith(Green)
   png.writeToFile("test2.png")
 
+  assert (encode(stringify(png)) == encode(rs), "not equal")
 
+when isMainModule and defined js:
+  import dom
+  
+  proc log*(str:varargs[auto]) = {.emit: "console.log(`str`);".}
+
+  var png = initPNG(3,3)
+  png.fillWith(Black)
+  log (stringify(png)==rs)
+  log stringify(png)
+  log rs
+  #var iel = document.createElement("IMG").ImageElement
+  #iel.src = stringify(png)#png.writebase64()
+  #log(iel.src)
+  #document.body.appendChild(iel)
 #proc color(rgba:uint32):Color = Color(rgba)
--- private/impl.nim
+++ private/impl.nim
@@ -40,8 +40,8 @@ proc crc32(sta: uint32, data:openarray[char]):uint32 =
   state = not state;
   for i in 0..<len:
     for j in 0..<8: # TODO: table-based
-      let bit : uint32 = (state xor (data[i].uint8 shr j.uint8)) and 1;
-      state = (state shr 1) xor ((high(uint32)+1-bit).int64 and 0xEDB88320).uint32
+      let bit : uint32 = (state xor (cast[uint8](data[i]) shr cast[uint8](j))) and 1;
+      state = (state shr 1) xor cast[uint32]( cast[int64](high(uint32)+1-bit) and 0xEDB88320 )
   return not state;
 
 proc adler32(data:openarray[char]):uint32 = # output is correct
@@ -49,10 +49,10 @@ proc adler32(data:openarray[char]):uint32 = # output is correct
   var s1 : uint32 = 1
   var s2 : uint32 = 0
   for i in 0..<len:
-    s1 = (s1 + data[i].uint32) mod 65521;
+    s1 = (s1 + cast[uint32](data[i])) mod 65521;
     s2 = (s2 + s1) mod 65521;
 
-  return s2.uint32 shl 16 or s1;
+  return cast[uint32](s2) shl 16 or s1;
 
 proc `&`[T](ar,ar2:openarray[T]):seq[T]=
   result = newSeq[T]()
@@ -68,12 +68,11 @@ proc bytesOf(b4:uint32):array[4,uint8]=
   result[3] = (b4 shr 0).uint8
 
 proc charsOf(b4:uint32):array[4,char]=
-  result[0] = (b4 shr 24).uint8.char
-  result[1] = (b4 shr 16).uint8.char
-  result[2] = (b4 shr 8).uint8.char
-  result[3] = (b4 shr 0).uint8.char
-  #echo repr result
-
+  result[0] = cast[uint8](b4 shr 24).char
+  result[1] = cast[uint8](b4 shr 16).char
+  result[2] = cast[uint8](b4 shr 8).char
+  result[3] = cast[uint8](b4 shr 0).char
+ 
 proc insertChars(s:var string, chrs:openarray[char])=
   if s==nil: s=""
   for c in chrs: s&=c
@@ -90,17 +89,17 @@ method serialize*(chunk: Chunk):string {.base.}=
   
     result.insertChars( charsOf(crc32(0, chunk.typ&chunk.data)))
   else: 
-    result&= charsOf(crc32(0, chunk.typ))
+    result.insertChars( charsOf(crc32(0, chunk.typ)))
  
 proc initIHDR*(w,h:int,bit_depth=8,ctype=6,comptype=0,ftype=0,itype=0):IHDR =
   # IHDR specific data
   result.width = w.uint32
   result.height = h.uint32
-  result.bit_depth = bit_depth.uint8
-  result.color_type = ctype.uint8
-  result.compression_type = comptype.uint8
-  result.filter_type = ftype.uint8
-  result.interlace_type = itype.uint8
+  result.bit_depth = cast[uint8](bit_depth)
+  result.color_type = cast[uint8](ctype)
+  result.compression_type = cast[uint8](comptype)
+  result.filter_type = cast[uint8](ftype)
+  result.interlace_type = cast[uint8](itype)
   # General chunk
   result.bytelen = 2*4 + 5
   result.typ = "IHDR"
@@ -138,24 +137,24 @@ proc applyFilter(pixbytes: var seq[seq[char]],filter:Filters= Filters.None)=
     for ln in pixbytes.mitems:
       var tmp = ln[0..3]
       for i in 4..<ln.len:
-        tmp &= (ln[i].uint8 - ln[i-4].uint8).char  
+        tmp &= (cast[uint8](ln[i]) - cast[uint8](ln[i-4])).char  
       ln = 1.char & tmp
   elif filter == Filters.Up:
   # Not working right, nice effect though
     for j in 0..<pixbytes.len:
       for i in 0..<pixbytes[j].len:
         if j==0: continue
-        pixbytes[j][i] = (pixbytes[j][i].uint8 - pixbytes[j-1][i].uint8).char
+        pixbytes[j][i] = ( cast[uint8](pixbytes[j][i]) - cast[uint8](pixbytes[j-1][i])).char
       if j==0: pixbytes[j] = 0.char & pixbytes[j]
       else: pixbytes[j] = 2.char & pixbytes[j]
 
 proc charsOfAndCompl(b2:uint16):array[4,char]=
   # Returns b2 and it's 1 complement, as 4 chars representing
   # two little endian uint16 
-  result[0] = (b2 shr 0).uint8.char
-  result[1] = (b2 shr 8).uint8.char
-  result[2] = ((b2 xor 0xFFFF) shr 0).uint8.char
-  result[3] = ((b2 xor 0xFFFF) shr 8).uint8.char
+  result[0] = cast[uint8](b2 shr 0).char
+  result[1] = cast[uint8](b2 shr 8).char
+  result[2] = cast[uint8]((b2 xor 0xFFFF) shr 0).char
+  result[3] = cast[uint8]((b2 xor 0xFFFF) shr 8).char
 
 #zlib magic headers
 #78 01 - No Compression/low
@@ -168,7 +167,7 @@ proc compress(pixbytes: string,isFirst:bool=true,isLast:bool=false) :seq[char] =
   if isFirst: result &= [120.char, 1.char]  #no compression  magic header
   if isLast: result &= 1.char else: result &= 0.char # set block delimiter if final
   
-  result &= charsOfAndCompl(pixbytes.len.uint16) # length
+  result &= charsOfAndCompl(cast[uint16](pixbytes.len)) # length
   result &= pixbytes # compressed data
   #result &= charsOf(adler32(pixbytes)) # adler32 MOVED TO initIDATs
 
@@ -202,12 +201,12 @@ proc initIDATs*(ihdr:IHDR, pixels:openarray[Color]):seq[IDAT] =
         result[i].data = compress(pixlines[i*block_size..<(i+1)*block_size],isFirst,isLast) # currently does not compress
       
       result[i].typ = "IDAT"
-      result[i].bytelen = (result[i].data.len).uint32
+      result[i].bytelen = cast[uint32](result[i].data.len)
   else:
     result[0].data = compress(pixlines,true,true) # currently does not compress
     result[0].data &= charsOf(adler32(pixlines)) # adler32
     result[0].typ = "IDAT"
-    result[0].bytelen = (result[0].data.len).uint32
+    result[0].bytelen = cast[uint8](result[0].data.len)
 
 proc initIEND*():IEND=
   result.bytelen = 0
