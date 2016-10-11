e9ee7dab36caeb5a295f23649a6a7ebd28c8c34f
Silvio T <stisa4@gmail.com>
2016-10-11 18:13:53 +0200

    export w,h

--- npng.nim
+++ npng.nim
@@ -20,18 +20,26 @@ type Color* = uint32
   ##  - solid blue:  ``0x0000FFFF``  
 
 const 
-  White* = Color(0xFFFFFFFF)
+  Transparent* = Color(0x00000000)
+    ## Common colors used for testing
   Black* = Color(0x000000FF)
-  Red* = Color(0xFF0000FF)
-  Green* = Color(0x00FF00FF)
   Blue* = Color(0x0000FFFF)
-  Yellow* = Color(0xFFFF00FF)
+  Green* = Color(0x00FF00FF)
+  Red* = Color(0xFF0000FF)
   Purple* = Color(0xFF00FFFF)
+  Yellow* = Color(0xFFFF00FF)
+  White* = Color(0xFFFFFFFF)
+  HalfTBlack* = Color(0x00000088) 
+    ## HalfT<Color> colors are <color> at half alpha
+  HalfTBlue* = Color(0x0000FF88)
+  HalfTGreen* = Color(0x00FF0088)
+  HalfTRed* = Color(0xFF000088)
+  HalftWhite* = Color(0xFFFFFF88)
 
 type PNG* = object
   ## A png file. To create one, see ``initPNG`` 
-  w,h:int
-  pixels:seq[Color]
+  w,h*:int
+  pixels*:seq[Color]
 
 proc initPNG*(w,h:int):PNG=
   ## Create a w by h png. Initially it is filled with black, fully transparent pixels.
